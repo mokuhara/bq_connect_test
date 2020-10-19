73f386fb1005295a2c14e92ed5ee01ad65fa30d6
MotokiKasaharaBansuke <motoki.kasahara@plaid.co.jp>
2020-10-19 23:23:59 +0900

    countUpコメントアウト

--- front/src/features/covid/Cards/Cards.tsx
+++ front/src/features/covid/Cards/Cards.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Cards.module.css";
-import CountUp from "react-countup";
+import CountUp from 'react-countup';
+import { useCountUp } from 'react-countup';
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
 
 import { GiHastyGrave } from "react-icons/gi";
@@ -16,44 +17,47 @@ const Cards: React.FC = () => {
     <Grid container spacing={1} justify="center">
       <Grid item xs={12} md={3} component={Card} className={styles.infected}>
         <Typography color="textSecondary" gutterBottom>
-          <MdLocalHospital />
-      peopleTested
+          <AiFillLike />
+      検査数
         </Typography>
         <Typography variant="h5">
-          <CountUp
+        {data[data.length - 1].peopleTested}
+          {/* <CountUp
             start={0}
             end={data[data.length - 1].peopleTested}
             duration={1.5}
             separator=","
-          />
+          /> */}
         </Typography>
       </Grid>
       <Grid item xs={12} md={3} component={Card} className={styles.recovered}>
         <Typography color="textSecondary" gutterBottom>
           <MdLocalHospital />
-          hospitalized
+          入院数
         </Typography>
         <Typography variant="h5">
-          <CountUp
+        {data[data.length - 1].hospitalized}
+          {/* <CountUp
             start={0}
             end={data[data.length - 1].hospitalized}
             duration={1.5}
             separator=","
-          />
+          /> */}
         </Typography>
       </Grid>
       <Grid item xs={12} md={3} component={Card} className={styles.deaths}>
         <Typography color="textSecondary" gutterBottom>
-          <MdLocalHospital />
-          deaths
+          <GiHastyGrave />
+          死亡者数
         </Typography>
         <Typography variant="h5">
-          <CountUp
+        {data[data.length - 1].deaths}
+          {/* <CountUp
             start={0}
             end={data[data.length - 1].deaths}
             duration={1.5}
             separator=","
-          />
+          /> */}
         </Typography>
       </Grid>
     </Grid>
